fc4f6c7f1f29b189e98a68023c17c7aced8720fa
Vladimir <noise123q@mail.ru>
2022-11-12 15:06:49 +0300

    add new page, correct style datepicker,

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -13,10 +13,31 @@ if (slider) {
         }
     });
     let valueNoSlider = [
-    document.querySelector(".js-value-lower"),
-    document.querySelector(".js-value-upper")
+    document.querySelectorAll(".js-value-lower")[0],
+    document.querySelectorAll(".js-value-upper")[0]
     ]
     slider.noUiSlider.on("update", function(values, handle) {
+        valueNoSlider[handle].innerHTML = Math.round(values[handle]);
+    })
+}
+
+
+//nuUiSlider
+let sliderSecond = document.querySelector('#sliderSecond');
+if (sliderSecond) {
+    noUiSlider.create(sliderSecond, {
+        start: [5000, 10000],
+        connect: true,
+        range: {
+            'min': 0,
+            'max': 15000
+        }
+    });
+    let valueNoSlider = [
+    document.querySelectorAll(".js-value-lower")[1],
+    document.querySelectorAll(".js-value-upper")[1]
+    ]
+    sliderSecond.noUiSlider.on("update", function(values, handle) {
         valueNoSlider[handle].innerHTML = Math.round(values[handle]);
     })
 }
